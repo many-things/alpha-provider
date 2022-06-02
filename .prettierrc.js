1e7a047a26a165524c36171026f1ed87fb460f94
Junho Yeo <i@junho.io>
2022-06-02 16:32:07 +0900

    Make Prettier work for Yarn Workspaces

--- .prettierrc.js
+++ .prettierrc.js
@@ -9,4 +9,5 @@ module.exports = {
   importOrder: ['<THIRD_PARTY_MODULES>', '@/(.*)$', '^[./](.*)$'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  plugins: [require('@trivago/prettier-plugin-sort-imports')],
 };
--- packages/example/src/home/HomePage.tsx
+++ packages/example/src/home/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HomePage = () => {
-  return <Container />;
+  return <Container></Container>;
 };
 
 export default HomePage;
