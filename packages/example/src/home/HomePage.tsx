790090e8d81c4881ddde3369921eec42078d0c43
Junho Yeo <i@junho.io>
2022-06-02 16:36:23 +0900

    [example] Implement wallet connecting

--- packages/example/src/home/HomePage.tsx
+++ packages/example/src/home/HomePage.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import { Web3Provider } from '@ethersproject/providers';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
+import Web3Modal from 'web3modal';
 
 const HomePage = () => {
-  return <Container></Container>;
+  const providerOptions = {
+    injected: {
+      display: {
+        name: 'Metamask',
+        description: 'Connect with MetaMask',
+      },
+      package: null,
+    },
+  };
+
+  const onClick = useCallback(async () => {
+    const web3Modal = new Web3Modal({
+      network: 'mainnet', // optional
+      cacheProvider: true, // optional
+      providerOptions, // required
+    });
+
+    const instance = await web3Modal.connect();
+
+    const provider = new Web3Provider(instance);
+    const accounts = await provider.listAccounts();
+    window.alert(JSON.stringify(accounts));
+  }, []);
+
+  return (
+    <Container>
+      <button onClick={onClick}>Connect Wallet</button>
+    </Container>
+  );
 };
 
 export default HomePage;
